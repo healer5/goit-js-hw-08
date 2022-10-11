b0ab12537d2ae2c16eb348ac19dc0f1eda735090
Olena Derevianko <olenka.derevianko@gmail.com>
2022-10-11 14:35:10 +0200

    upd task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,11 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const PLAYER_CURENT_TIME = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(timeUpDate, 1000));
 function timeUpDate(data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
+  localStorage.setItem(PLAYER_CURENT_TIME, data.seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(localStorage.getItem(PLAYER_CURENT_TIME));
